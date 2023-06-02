fff9fdc7f70c10d6d7c8c034a801fda9256ba215
Jason <github@jasongetsemail.com>
2023-06-01 20:09:45 -0400

    Added styling and sounds to the buttons. Currently the same sound plays for all buttons.

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,9 @@
 let buttons = document.querySelectorAll(".drum");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    alert("Clicked");
+    var buttonClicked = this;
+    buttonClicked.style.color = "white";
+    var sound = new Audio("./sounds/tom-1.mp3");
+    sound.play();
   });
 }
