7e5a3c979328f7f5b9c1aeffcd037f9cc68b3216
Jason <github@jasongetsemail.com>
2023-06-02 10:14:32 -0400

    Mapped key presses to the correct sounds. reworked the click listener to keep things dry

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,31 +1,37 @@
 let buttons = document.querySelectorAll(".drum");
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    var buttonClicked = this;
-    var sound;
-    switch (buttonClicked.innerHTML) {
-      case "w":
-        sound = new Audio("./sounds/crash.mp3");
-        break;
-      case "a":
-        sound = new Audio("./sounds/kick-bass.mp3");
-        break;
-      case "s":
-        sound = new Audio("./sounds/snare.mp3");
-        break;
-      case "d":
-        sound = new Audio("./sounds/tom-1.mp3");
-        break;
-      case "j":
-        sound = new Audio("./sounds/tom-2.mp3");
-        break;
-      case "k":
-        sound = new Audio("./sounds/tom-3.mp3");
-        break;
-      case "l":
-        sound = new Audio("./sounds/tom-4.mp3");
-        break;
-    }
-    sound.play();
+  buttons[i].addEventListener("keydown", function (event) {
+    mapSounds(event.key);
   });
+  buttons[i].addEventListener("click", function () {
+    mapSounds(this.innerHTML);
+  });
+}
+
+function mapSounds(buttonClicked) {
+  var sound;
+  switch (buttonClicked) {
+    case "w":
+      sound = new Audio("./sounds/crash.mp3");
+      break;
+    case "a":
+      sound = new Audio("./sounds/kick-bass.mp3");
+      break;
+    case "s":
+      sound = new Audio("./sounds/snare.mp3");
+      break;
+    case "d":
+      sound = new Audio("./sounds/tom-1.mp3");
+      break;
+    case "j":
+      sound = new Audio("./sounds/tom-2.mp3");
+      break;
+    case "k":
+      sound = new Audio("./sounds/tom-3.mp3");
+      break;
+    case "l":
+      sound = new Audio("./sounds/tom-4.mp3");
+      break;
+  }
+  sound.play();
 }
